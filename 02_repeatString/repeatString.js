98c9a3604fadab081376cab36990f08587173e48
Hellscr3am <96802088+hellscr34m@users.noreply.github.com>
2023-02-16 21:24:44 -0500

    Done with exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function repeatString(str, num) {
+    let varResult = '';
+    for (let i = 0; i < num; i++) {
+        varResult += str;
+    }
+    if (num < 0) {
+        return 'ERROR';
+    }
+    else {
+        return varResult;
+    }
 };
 
 // Do not edit below this line
