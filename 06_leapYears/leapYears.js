80454945c7ea96e8ae8760b1e5bd83a9d5c4beaa
Hellscr3am <96802088+hellscr34m@users.noreply.github.com>
2023-02-19 21:05:55 -0500

    exercise 06 completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(myYear) {
+    if (isDivisibleBy(myYear, 100)) {
+        if (isDivisibleBy(myYear,400)) {
+            return true;
+        }
+        else
+            return false;
+    }
+    else if (isDivisibleBy(myYear,4)) {
+        return true;
+    }
+    else return false;
 };
+function isDivisibleBy(number, divisor) {
+    return number % divisor === 0;
+}
 
 // Do not edit below this line
 module.exports = leapYears;
