2a749d2cf60519a43254a431d96d54c1201ef37e
Hellscr3am <96802088+hellscr34m@users.noreply.github.com>
2023-02-19 21:19:05 -0500

    Completed exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(vFahrenheit) {
+  return roundToOneDecimal(vCelsius = (vFahrenheit - 32) * 5 / 9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(vCelsius) {
+  return roundToOneDecimal(vFahrenheit = (vCelsius * 9 / 5) + 32);
 };
+function roundToOneDecimal(num) {
+  return parseFloat(num.toFixed(1));
+}
 
 // Do not edit below this line
 module.exports = {
