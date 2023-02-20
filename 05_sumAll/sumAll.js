cbe5c3785c7d2d6aabe7459a9df6f4b1ff078ba3
Hellscr3am <96802088+hellscr34m@users.noreply.github.com>
2023-02-19 20:48:44 -0500

    exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(lowInt, highInt) {
+    varResult = 0
+    if (lowInt < 0 || highInt < 0) {
+        return 'ERROR';
+    }
+    else if (typeof lowInt !== 'number' || typeof highInt !== 'number') {
+        return 'ERROR';
+    }
+    else {
+        let start = Math.min(lowInt, highInt);
+        let end = Math.max(lowInt, highInt);
+        for (let i = start; i <= end; i++) {
+            varResult += i;
+        }
+        return varResult;
+    }
 };
 
 // Do not edit below this line
